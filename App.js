ee8947b1a76244b067c28bf189e846488b363d76
guhankesav <47454368+guhankesav@users.noreply.github.com>
2021-04-03 22:49:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,157 @@
+// import logo from './logo.svg';
+// import './App.css';
+// // import {
+// //   Navbar,
+// //   Nav,
+// //   NavDropdown,
+// //   Form,
+// //   FormControl,
+// //   Button,
+// // } from "react-bootstrap";
+// // window.onscroll = function() {myFunction()};
+
+// // // Get the navbar
+// // var navbar = document.getElementById("navbar");
+
+// // // Get the offset position of the navbar
+// // var sticky = navbar.offsetTop;
+
+// // // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// // function myFunction() {
+// //   if (window.pageYOffset >= sticky) {
+// //     navbar.classList.add("sticky")
+// //   } else {
+// //     navbar.classList.remove("sticky");
+// //   }
+// // }
+// function App() {
+//   return (
+//     <div className="App">
+    
+//       <div class="header">
+//         <h2>Scroll Down</h2>
+//         <p>Scroll down to see the sticky effect.</p>
+//       </div>
+//       <div id="navbar">
+//       <p>The navbar will stick to the top when you reach its scroll position.</p>
+//         <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+    
+//       </div>
+//       <div class="content">
+//         <h3>Sticky Navigation Example</h3>
+//         <p>The navbar will stick to the top when you reach its scroll position.</p>
+//         <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+//         <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+//         <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+//         <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+//         <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+//         <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+//         <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+//       </div>
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import logo from './logo.svg';
+import './App.css';
+// function App() {
+//   return (
+//     <div className="App">
+    
+//       <div class="header">
+//         <h2>Scroll Down</h2>
+//         <p>Scroll down to see the sticky effect.</p>
+//       </div>
+//       <div id="navbar">
+//       <p>The navbar will stick to the top when you reach its scroll position.</p>
+//         <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+    
+//       </div>
+//       <div class="content">
+//         <h3>Sticky Navigation Example</h3>
+//         <p>The navbar will stick to the top when you reach its scroll position.</p>
+//         <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+//         <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+//         <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+//         <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+//         <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+//         <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+//         <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+//       </div>
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// import React from 'react';
+import './index.css';
+import "./index.css";
+import * as React from "react";
+import {
+  ScheduleComponent,
+  ViewsDirective,
+  ViewDirective,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+  Resize,
+  DragAndDrop
+} from "@syncfusion/ej2-react-schedule";
+
+import { extend } from "@syncfusion/ej2-base";
+import { SampleBase } from "./sample-base";
+import * as dataSource from "./datasource.json";
+
+ class App extends SampleBase {
+  constructor() {
+    super(...arguments);
+    this.data = extend([], dataSource.scheduleData, null, true);
+  }
+  render() {
+    return (
+      <div className="schedule-control-section">
+        <div className="col-lg-9 control-section">
+          <div className="control-wrapper">
+            <ScheduleComponent
+              height="650px"
+              ref={schedule => (this.scheduleObj = schedule)}
+              selectedDate={new Date(2021, 0, 10)}
+              eventSettings={{ dataSource: this.data }}
+            >
+              <ViewsDirective>
+                <ViewDirective option="Day" />
+                <ViewDirective option="Week" />
+                <ViewDirective option="WorkWeek" />
+                <ViewDirective option="Month" />
+                <ViewDirective option="Agenda" />
+              </ViewsDirective>
+              <Inject
+                services={[
+                  Day,
+                  Week,
+                  WorkWeek,
+                  Month,
+                  Agenda,
+                  Resize,
+                  DragAndDrop
+                ]}
+              />
+            </ScheduleComponent>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
+
